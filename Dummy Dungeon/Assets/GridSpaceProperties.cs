34def2a3a7f4cac001deb0e4024f1737555eafa3
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-06-08 00:14:03 -0400

    Created pathfinding arrow sprite for future debugging purposes.

--- Dummy Dungeon/Assets/GridSpaceProperties.cs
+++ Dummy Dungeon/Assets/GridSpaceProperties.cs
@@ -9,11 +9,19 @@ public class GridSpaceProperties : MonoBehaviour
 	public int moveCost = 1; // the movement cost to traverse this spot
 
 	// Sprite representation
-	public SpriteRenderer sr;
-	public bool 32x16; //true if the sprite is 32x16 otherwise assumed to be 16x16
+	private SpriteRenderer sr;
+	//public bool Tall; //true if the sprite is 32x16 otherwise assumed to be 16x16
 
 	void Awake () 
 	{
 		sr = GetComponent<SpriteRenderer>();
+	}
+
+	void Start()
+	{
+		if (!impassible)
+		{
+			sr.enabled = false;
+		}
 	}
 }
