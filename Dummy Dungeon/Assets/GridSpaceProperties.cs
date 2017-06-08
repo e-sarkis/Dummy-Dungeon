59db62741ff44a4e150c79ac4d8987f6d0dae956
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-06-07 23:06:29 -0400

    Created GridSpaceProperties script and began variable initialization.

---
+++ Dummy Dungeon/Assets/GridSpaceProperties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpaceProperties : MonoBehaviour 
+{
+	// Movement and pathfinding
+	public bool impassible = false; // false if this location legally traversible
+	public int moveCost = 1; // the movement cost to traverse this spot
+
+	// Sprite representation
+	public SpriteRenderer sr;
+	public bool 32x16; //true if the sprite is 32x16 otherwise assumed to be 16x16
+
+	void Awake () 
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+}
